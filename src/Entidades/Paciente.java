8f5a266e77d0609a3baae12cefb3d973114573c3
Mikel <Mikel@172.16.5.127>
2020-11-10 19:59:27 +0100

    Nueva version de proyecto

---
+++ src/Entidades/Paciente.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Mikel
+ */
+public class Paciente {
+    protected long id;
+    protected String nombre;
+    protected String apellidos;
+    protected String telefono;
+    protected String direccion;
+    protected String DNI;
+}
---
+++ src/Entidades/Tratamiento.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Mikel
+ */
+public class Tratamiento {
+    protected long id;
+    protected String nombre_descriptivo;
+    protected Date fecha_inicio;
+    
+}

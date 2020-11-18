d02f7bb025c8ed95dbb96045bc7c41c4c532b2c2
Mikel <Mikel@172.16.5.127>
2020-11-18 20:30:44 +0100

    clases con getters ans setters

--- src/Entidades/Cita.java
+++ src/Entidades/Cita.java
@@ -13,6 +13,44 @@ import java.sql.Time;
 public class Cita {
     protected long id;
     protected Date fecha;
-    protected char horario;
+    protected char rango_horario;
     protected Time hora;
+
+    
+    
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public char getRango_horario() {
+        return rango_horario;
+    }
+
+    public void setRango_horario(char rango_horario) {
+        this.rango_horario = rango_horario;
+    }
+
+    public Time getHora() {
+        return hora;
+    }
+
+    public void setHora(Time hora) {
+        this.hora = hora;
+    }
+    
+    
+    
 }

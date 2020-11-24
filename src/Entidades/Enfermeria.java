563f38994af63e2974fa603b51d9cd06da9e1cd2
AGuadix <BrookS@DESKTOP-RIQSDE6>
2020-11-24 13:16:44 +0100

    Clase Enfermeria  creada. Atributos añadidos. Getters y setters creados. Constructor por defecto y contructor de copia creados.

---
+++ src/Entidades/Enfermeria.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author AGuadix
+ */
+public class Enfermeria extends Empleado{
+    private char categoria;
+
+    //Getters & Setters
+    public char getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(char categoria) {
+        this.categoria = categoria;
+    }
+    
+    //Contructor por defecto con llamada la superclase
+    public Enfermeria(){
+        super();
+    }
+    
+    //Constructor de copia
+    public Enfermeria(Enfermeria e){
+        this.categoria = e.getCategoria();
+    }
+}
